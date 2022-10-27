ae9453e954c28b6edf967561d1c0af53bc242bd7
SFelbeck <stefanfelbeck@gmail.com>
2022-10-26 17:03:15 -0700

    routing added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React from "react";
+import MainPage from "./components/MainPage";
+import IntroPage from "./components/IntroScreen";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import Header from "./components/Header";
+import Layout from "./components/Layout";
 
 
 //import routes
@@ -11,7 +17,15 @@ import {
 function App() {
   return (
     <BrowserRouter>
-    
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<MainPage />} />
+          
+        </Route>
+        <Route path="/IntroScreen" element={<IntroPage />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Signup" element={<SignUp />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function Header() {
     return(
         <header class="header">
-            <img src="./assets/LogoIcon.png" alt="MinMax logo" />
+            <img src={require("./assets/LogoIcon.png")} alt="MinMax logo" width="50px" height="50px" />
             <input class="search-input" type="text" placeholder="Search..." />
             <button class="search-btn">Search</button>
             <div>
